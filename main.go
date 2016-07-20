813265324afa803c947aed92dc9e8175c18b697a
백종열 <geeksbaek@gmail.com>
2016-07-21 01:18:24 +0900

    rename

--- main.go
+++ main.go
@@ -11,21 +11,21 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-// ProxySouece 구조체는 프록시를 가져올 Source와 Parser 함수로
+// ProxyBuilder 구조체는 프록시를 가져올 Source와 Parser 함수로
 // 이루어져 있습니다. Source는 여러 개일 수 있으나, 단일한 Parser 함수로
 // 호환 가능한 형태여야 합니다. Parser는 func(source string) []*url.URL 형태를
 // 충족시켜야 합니다.
-type ProxySouece struct {
+type ProxyBuilder struct {
 	Source []string
 	Parser func(source string) []*url.URL
 }
 
-// ProxyFactory 구조체는 복수의 ProxySouece를 가집니다.
+// ProxyFactory 구조체는 복수의 ProxySource를 가집니다.
 type ProxyFactory struct {
-	ProxySources []*ProxySouece
+	ProxyBuilders []*ProxyBuilder
 }
 
-var defaultProxySources = []*ProxySouece{{
+var defaultProxySources = []*ProxyBuilder{{
 	Source: []string{
 		"https://incloak.com/proxy-list/?type=hs#list",
 		"https://incloak.com/proxy-list/?type=hs&start=64#list",
@@ -96,23 +96,23 @@ var defaultProxySources = []*ProxySouece{{
 	},
 }}
 
-// NewProxySource 함수는 빈 ProxySouece 구조체를 반환합니다.
+// NewProxySource 함수는 빈 ProxySource 구조체를 반환합니다.
 func NewProxySource() *ProxyFactory {
 	return &ProxyFactory{}
 }
 
 // AddDefaultOpenProxySources 함수는 패키지에 포함되어 있는
-// 기본 ProxySouece들을 추가합니다.
+// 기본 ProxySource들을 추가합니다.
 func (pf *ProxyFactory) AddDefaultOpenProxySources() {
 	for _, defaultProxySource := range defaultProxySources {
-		pf.ProxySources = append(pf.ProxySources, defaultProxySource)
+		pf.ProxyBuilders = append(pf.ProxyBuilders, defaultProxySource)
 	}
 }
 
-// AddCustomProxySource 함수는 사용자가 구현한 ProxySouece를
+// AddCustomProxySource 함수는 사용자가 구현한 ProxyBuilder를
 // 인자로 받아 ProxyFactory 객체에 추가합니다.
-func (pf *ProxyFactory) AddCustomProxySource(proxySouece *ProxySouece) {
-	pf.ProxySources = append(pf.ProxySources, proxySouece)
+func (pf *ProxyFactory) AddCustomProxySource(proxyBuilder *ProxyBuilder) {
+	pf.ProxyBuilders = append(pf.ProxyBuilders, proxyBuilder)
 }
 
 // ProxySource 함수는 프록시를 전송하는 채널을 반환합니다. 이 채널은 파싱된
@@ -123,12 +123,12 @@ func (pf *ProxyFactory) ProxySource() chan *url.URL {
 
 	go func() {
 		wg := new(sync.WaitGroup)
-		for _, proxySource := range pf.ProxySources {
-			for _, source := range proxySource.Source {
+		for _, ProxyBuilder := range pf.ProxyBuilders {
+			for _, source := range ProxyBuilder.Source {
 				wg.Add(1)
 				go func(source string) {
 					defer wg.Done()
-					for _, proxy := range proxySource.Parser(source) {
+					for _, proxy := range ProxyBuilder.Parser(source) {
 						bufCh <- proxy
 					}
 				}(source)
